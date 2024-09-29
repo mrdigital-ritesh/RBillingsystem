05fde2748079eb7eff61312bc4743dd21dc1fc2a
bagdi <111894690+Bagd21@users.noreply.github.com>
2024-09-29 18:13:35 +0530

    today 29 sept done

--- product.Designer.vb
+++ product.Designer.vb
@@ -24,25 +24,29 @@ Partial Class product
     Private Sub InitializeComponent()
         DataGridView1 = New DataGridView()
         Panel1 = New Panel()
+        stockdate = New TextBox()
+        Label11 = New Label()
+        disc = New TextBox()
+        Label10 = New Label()
+        category = New ComboBox()
+        Label9 = New Label()
         Button3 = New Button()
-        Button4 = New Button()
         Button2 = New Button()
         Button1 = New Button()
-        TextBox8 = New TextBox()
+        supplier = New TextBox()
         Label8 = New Label()
-        TextBox7 = New TextBox()
+        qty = New TextBox()
         Label7 = New Label()
-        TextBox6 = New TextBox()
         Label6 = New Label()
-        TextBox5 = New TextBox()
+        mrp = New TextBox()
         Label5 = New Label()
-        TextBox4 = New TextBox()
+        purprice = New TextBox()
         Label4 = New Label()
-        TextBox3 = New TextBox()
+        pbrand = New TextBox()
         Label3 = New Label()
-        TextBox2 = New TextBox()
+        pname = New TextBox()
         Label2 = New Label()
-        TextBox1 = New TextBox()
+        pid = New TextBox()
         Label1 = New Label()
         CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
         Panel1.SuspendLayout()
@@ -53,9 +57,9 @@ Partial Class product
         DataGridView1.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
         DataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill
         DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        DataGridView1.Location = New Point(12, 181)
+        DataGridView1.Location = New Point(12, 382)
         DataGridView1.Name = "DataGridView1"
-        DataGridView1.Size = New Size(1019, 338)
+        DataGridView1.Size = New Size(1121, 160)
         DataGridView1.TabIndex = 16
         ' 
         ' Panel1
@@ -63,192 +67,255 @@ Partial Class product
         Panel1.Anchor = AnchorStyles.Top Or AnchorStyles.Left Or AnchorStyles.Right
         Panel1.AutoSize = True
         Panel1.BackColor = SystemColors.GradientActiveCaption
+        Panel1.Controls.Add(stockdate)
+        Panel1.Controls.Add(Label11)
+        Panel1.Controls.Add(disc)
+        Panel1.Controls.Add(Label10)
+        Panel1.Controls.Add(category)
+        Panel1.Controls.Add(Label9)
         Panel1.Controls.Add(Button3)
-        Panel1.Controls.Add(Button4)
         Panel1.Controls.Add(Button2)
         Panel1.Controls.Add(Button1)
-        Panel1.Controls.Add(TextBox8)
+        Panel1.Controls.Add(supplier)
         Panel1.Controls.Add(Label8)
-        Panel1.Controls.Add(TextBox7)
+        Panel1.Controls.Add(qty)
         Panel1.Controls.Add(Label7)
-        Panel1.Controls.Add(TextBox6)
         Panel1.Controls.Add(Label6)
-        Panel1.Controls.Add(TextBox5)
+        Panel1.Controls.Add(mrp)
         Panel1.Controls.Add(Label5)
-        Panel1.Controls.Add(TextBox4)
+        Panel1.Controls.Add(purprice)
         Panel1.Controls.Add(Label4)
-        Panel1.Controls.Add(TextBox3)
+        Panel1.Controls.Add(pbrand)
         Panel1.Controls.Add(Label3)
-        Panel1.Controls.Add(TextBox2)
+        Panel1.Controls.Add(pname)
         Panel1.Controls.Add(Label2)
-        Panel1.Controls.Add(TextBox1)
+        Panel1.Controls.Add(pid)
         Panel1.Controls.Add(Label1)
         Panel1.Location = New Point(12, 12)
         Panel1.Name = "Panel1"
-        Panel1.Size = New Size(1019, 163)
+        Panel1.Size = New Size(1121, 364)
         Panel1.TabIndex = 17
+        ' 
+        ' stockdate
+        ' 
+        stockdate.Font = New Font("Segoe UI", 12F)
+        stockdate.Location = New Point(791, 300)
+        stockdate.Name = "stockdate"
+        stockdate.Size = New Size(160, 29)
+        stockdate.TabIndex = 40
+        ' 
+        ' Label11
+        ' 
+        Label11.AutoSize = True
+        Label11.Font = New Font("Segoe UI", 12F)
+        Label11.Location = New Point(666, 300)
+        Label11.Name = "Label11"
+        Label11.Size = New Size(65, 21)
+        Label11.TabIndex = 39
+        Label11.Text = "Label11"
+        ' 
+        ' disc
+        ' 
+        disc.Font = New Font("Segoe UI", 12F)
+        disc.Location = New Point(417, 245)
+        disc.Name = "disc"
+        disc.Size = New Size(160, 29)
+        disc.TabIndex = 38
+        ' 
+        ' Label10
+        ' 
+        Label10.AutoSize = True
+        Label10.Font = New Font("Segoe UI", 12F)
+        Label10.Location = New Point(228, 300)
+        Label10.Name = "Label10"
+        Label10.Size = New Size(65, 21)
+        Label10.TabIndex = 37
+        Label10.Text = "Label10"
+        ' 
+        ' category
+        ' 
+        category.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        category.FormattingEnabled = True
+        category.Location = New Point(791, 141)
+        category.Name = "category"
+        category.Size = New Size(160, 29)
+        category.TabIndex = 36
+        ' 
+        ' Label9
+        ' 
+        Label9.Anchor = AnchorStyles.Top Or AnchorStyles.Left Or AnchorStyles.Right
+        Label9.AutoSize = True
+        Label9.BackColor = Color.White
+        Label9.Font = New Font("Rockwell", 14.25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label9.Location = New Point(593, 12)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(69, 21)
+        Label9.TabIndex = 35
+        Label9.Text = "Label9"
         ' 
         ' Button3
         ' 
-        Button3.Location = New Point(351, 115)
+        Button3.Anchor = AnchorStyles.Top Or AnchorStyles.Right
+        Button3.Location = New Point(1023, 86)
         Button3.Name = "Button3"
-        Button3.Size = New Size(75, 23)
+        Button3.Size = New Size(86, 34)
         Button3.TabIndex = 34
         Button3.Text = "Button3"
         Button3.UseVisualStyleBackColor = True
         ' 
-        ' Button4
-        ' 
-        Button4.Location = New Point(242, 115)
-        Button4.Name = "Button4"
-        Button4.Size = New Size(75, 23)
-        Button4.TabIndex = 33
-        Button4.Text = "Button4"
-        Button4.UseVisualStyleBackColor = True
-        ' 
         ' Button2
         ' 
-        Button2.Location = New Point(120, 115)
+        Button2.Anchor = AnchorStyles.Top Or AnchorStyles.Right
+        Button2.Location = New Point(1023, 147)
         Button2.Name = "Button2"
-        Button2.Size = New Size(75, 23)
+        Button2.Size = New Size(86, 34)
         Button2.TabIndex = 32
         Button2.Text = "Button2"
         Button2.UseVisualStyleBackColor = True
         ' 
         ' Button1
         ' 
-        Button1.Location = New Point(11, 115)
+        Button1.Anchor = AnchorStyles.Top Or AnchorStyles.Right
+        Button1.Location = New Point(1023, 29)
         Button1.Name = "Button1"
-        Button1.Size = New Size(75, 23)
+        Button1.Size = New Size(86, 34)
         Button1.TabIndex = 18
         Button1.Text = "Button1"
         Button1.UseVisualStyleBackColor = True
         ' 
-        ' TextBox8
+        ' supplier
         ' 
-        TextBox8.Location = New Point(884, 63)
-        TextBox8.Name = "TextBox8"
-        TextBox8.Size = New Size(100, 23)
-        TextBox8.TabIndex = 31
+        supplier.Font = New Font("Segoe UI", 12F)
+        supplier.Location = New Point(418, 300)
+        supplier.Name = "supplier"
+        supplier.Size = New Size(160, 29)
+        supplier.TabIndex = 31
         ' 
         ' Label8
         ' 
         Label8.AutoSize = True
-        Label8.Location = New Point(775, 66)
+        Label8.Font = New Font("Segoe UI", 12F)
+        Label8.Location = New Point(666, 248)
         Label8.Name = "Label8"
-        Label8.Size = New Size(41, 15)
+        Label8.Size = New Size(56, 21)
         Label8.TabIndex = 30
         Label8.Text = "Label8"
         ' 
-        ' TextBox7
+        ' qty
         ' 
-        TextBox7.Location = New Point(638, 63)
-        TextBox7.Name = "TextBox7"
-        TextBox7.Size = New Size(100, 23)
-        TextBox7.TabIndex = 29
+        qty.Font = New Font("Segoe UI", 12F)
+        qty.Location = New Point(791, 245)
+        qty.Name = "qty"
+        qty.Size = New Size(160, 29)
+        qty.TabIndex = 29
         ' 
         ' Label7
         ' 
         Label7.AutoSize = True
-        Label7.Location = New Point(529, 66)
+        Label7.Font = New Font("Segoe UI", 12F)
+        Label7.Location = New Point(228, 248)
         Label7.Name = "Label7"
-        Label7.Size = New Size(41, 15)
+        Label7.Size = New Size(56, 21)
         Label7.TabIndex = 28
         Label7.Text = "Label7"
-        ' 
-        ' TextBox6
-        ' 
-        TextBox6.Location = New Point(368, 63)
-        TextBox6.Name = "TextBox6"
-        TextBox6.Size = New Size(100, 23)
-        TextBox6.TabIndex = 27
         ' 
         ' Label6
         ' 
         Label6.AutoSize = True
-        Label6.Location = New Point(259, 66)
+        Label6.Font = New Font("Segoe UI", 12F)
+        Label6.Location = New Point(666, 203)
         Label6.Name = "Label6"
-        Label6.Size = New Size(41, 15)
+        Label6.Size = New Size(56, 21)
         Label6.TabIndex = 26
         Label6.Text = "Label6"
         ' 
-        ' TextBox5
+        ' mrp
         ' 
-        TextBox5.Location = New Point(120, 63)
-        TextBox5.Name = "TextBox5"
-        TextBox5.Size = New Size(100, 23)
-        TextBox5.TabIndex = 25
+        mrp.Font = New Font("Segoe UI", 12F)
+        mrp.Location = New Point(791, 195)
+        mrp.Name = "mrp"
+        mrp.Size = New Size(158, 29)
+        mrp.TabIndex = 25
         ' 
         ' Label5
         ' 
         Label5.AutoSize = True
-        Label5.Location = New Point(11, 66)
+        Label5.Font = New Font("Segoe UI", 12F)
+        Label5.Location = New Point(228, 203)
         Label5.Name = "Label5"
-        Label5.Size = New Size(41, 15)
+        Label5.Size = New Size(56, 21)
         Label5.TabIndex = 24
         Label5.Text = "Label5"
         ' 
-        ' TextBox4
+        ' purprice
         ' 
-        TextBox4.Location = New Point(884, 22)
-        TextBox4.Name = "TextBox4"
-        TextBox4.Size = New Size(100, 23)
-        TextBox4.TabIndex = 23
+        purprice.Font = New Font("Segoe UI", 12F)
+        purprice.Location = New Point(418, 195)
+        purprice.Name = "purprice"
+        purprice.Size = New Size(160, 29)
+        purprice.TabIndex = 23
         ' 
         ' Label4
         ' 
         Label4.AutoSize = True
-        Label4.Location = New Point(775, 25)
+        Label4.Font = New Font("Segoe UI", 12F)
+        Label4.Location = New Point(666, 149)
         Label4.Name = "Label4"
-        Label4.Size = New Size(41, 15)
+        Label4.Size = New Size(56, 21)
         Label4.TabIndex = 22
         Label4.Text = "Label4"
         ' 
-        ' TextBox3
+        ' pbrand
         ' 
-        TextBox3.Location = New Point(638, 22)
-        TextBox3.Name = "TextBox3"
-        TextBox3.Size = New Size(100, 23)
-        TextBox3.TabIndex = 21
+        pbrand.Font = New Font("Segoe UI", 12F)
+        pbrand.Location = New Point(418, 138)
+        pbrand.Name = "pbrand"
+        pbrand.Size = New Size(158, 29)
+        pbrand.TabIndex = 21
         ' 
         ' Label3
         ' 
         Label3.AutoSize = True
-        Label3.Location = New Point(529, 25)
+        Label3.Font = New Font("Segoe UI", 12F)
+        Label3.Location = New Point(228, 149)
         Label3.Name = "Label3"
-        Label3.Size = New Size(41, 15)
+        Label3.Size = New Size(56, 21)
         Label3.TabIndex = 20
         Label3.Text = "Label3"
         ' 
-        ' TextBox2
+        ' pname
         ' 
-        TextBox2.Location = New Point(368, 22)
-        TextBox2.Name = "TextBox2"
-        TextBox2.Size = New Size(100, 23)
-        TextBox2.TabIndex = 19
+        pname.Font = New Font("Segoe UI", 12F)
+        pname.Location = New Point(791, 91)
+        pname.Name = "pname"
+        pname.Size = New Size(160, 29)
+        pname.TabIndex = 19
         ' 
         ' Label2
         ' 
         Label2.AutoSize = True
-        Label2.Location = New Point(259, 25)
+        Label2.Font = New Font("Segoe UI", 12F)
+        Label2.Location = New Point(666, 99)
         Label2.Name = "Label2"
-        Label2.Size = New Size(41, 15)
+        Label2.Size = New Size(56, 21)
         Label2.TabIndex = 18
         Label2.Text = "Label2"
         ' 
-        ' TextBox1
+        ' pid
         ' 
-        TextBox1.Location = New Point(120, 22)
-        TextBox1.Name = "TextBox1"
-        TextBox1.Size = New Size(100, 23)
-        TextBox1.TabIndex = 17
+        pid.Font = New Font("Segoe UI", 12F)
+        pid.Location = New Point(418, 91)
+        pid.Name = "pid"
+        pid.Size = New Size(159, 29)
+        pid.TabIndex = 17
         ' 
         ' Label1
         ' 
         Label1.AutoSize = True
-        Label1.Location = New Point(11, 25)
+        Label1.Font = New Font("Segoe UI", 12F)
+        Label1.Location = New Point(228, 99)
         Label1.Name = "Label1"
-        Label1.Size = New Size(41, 15)
+        Label1.Size = New Size(56, 21)
         Label1.TabIndex = 16
         Label1.Text = "Label1"
         ' 
@@ -257,10 +324,11 @@ Partial Class product
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
         AutoSize = True
-        ClientSize = New Size(1043, 531)
+        ClientSize = New Size(1145, 554)
         Controls.Add(Panel1)
         Controls.Add(DataGridView1)
         Name = "product"
+        StartPosition = FormStartPosition.CenterScreen
         Text = "product"
         CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
         Panel1.ResumeLayout(False)
@@ -270,25 +338,29 @@ Partial Class product
     End Sub
     Friend WithEvents DataGridView1 As DataGridView
     Friend WithEvents Panel1 As Panel
-    Friend WithEvents TextBox8 As TextBox
+    Friend WithEvents supplier As TextBox
     Friend WithEvents Label8 As Label
-    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents qty As TextBox
     Friend WithEvents Label7 As Label
-    Friend WithEvents TextBox6 As TextBox
     Friend WithEvents Label6 As Label
-    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents mrp As TextBox
     Friend WithEvents Label5 As Label
-    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents purprice As TextBox
     Friend WithEvents Label4 As Label
-    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents pbrand As TextBox
     Friend WithEvents Label3 As Label
-    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents pname As TextBox
     Friend WithEvents Label2 As Label
-    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents pid As TextBox
     Friend WithEvents Label1 As Label
     Friend WithEvents Button1 As Button
     Friend WithEvents Button3 As Button
-    Friend WithEvents Button4 As Button
     Friend WithEvents Button2 As Button
+    Friend WithEvents Label9 As Label
+    Friend WithEvents category As ComboBox
+    Friend WithEvents disc As TextBox
+    Friend WithEvents Label10 As Label
+    Friend WithEvents stockdate As TextBox
+    Friend WithEvents Label11 As Label
 
 End Class
--- product.vb
+++ product.vb
@@ -1,5 +1,7 @@
 ﻿Imports MySql.Data.MySqlClient
 Public Class product
+    Dim qry As String
+
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         'Call connect()
         Dim screenWidth As Integer = Screen.PrimaryScreen.Bounds.Width
@@ -11,23 +13,39 @@ Public Class product
         Label1.Text = "Product Id:"
         Label2.Text = "Product Name:"
         Label3.Text = "Product Brand:"
-        Label6.Text = "Category Id:"
+        Label6.Text = "MRP:"
         Label5.Text = "Purchase Price:"
-        Label4.Text = "MRP:"
-        Label7.Text = "Quantity:"
-        Label8.Text = "Supplier Id:"
-        TextBox1.Enabled = False
+        Label4.Text = "Category:"
+        Label8.Text = "Quantity:"
+        Label7.Text = "Discount:"
+        Label10.Text = "Supplier Name:"
+        Label11.Text = "Stock Date:"
+        Label9.Text = "R-MART (PRODUCT)"
+
+        Button1.Text = "Add Item"
+        Button2.Text = "Update Item"
+        Button3.Text = "Delete Item"
+        Button2.Enabled = False
+        Button3.Enabled = False
+
+
+        pid.Enabled = False
+        stockdate.Enabled = False
+        stockdate.Text = Today.ToString("yyyy-MM-dd")
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Call showdata()
+        Call autoid()
+        Call loadcat()
+        pname.Focus()
 
     End Sub
 
     Public Sub showdata()
         Call connect()
-        Dim str As String
-        str = "select * from products"
-        Dim da As New MySqlDataAdapter(str, conn)
-        Dim dt As New DataTable("product_info")
+        qry = "select * from products"
+        Dim da As New MySqlDataAdapter(qry, conn)
+        Dim dt As New DataTable()
+
         da.Fill(dt)
 
         DataGridView1.DataSource = dt
@@ -35,5 +53,76 @@ Public Class product
         conn.Close()
     End Sub
 
+    Public Sub autoid()
+        Call connect()
+        qry = "select max(pro_id) from products"
+        Dim da As New MySqlDataAdapter(qry, conn)
+        Dim dt As New DataTable()
 
+        da.Fill(dt)
+        Dim pro_id As Double
+        If dt.Rows.Count > 0 AndAlso Not IsDBNull(dt.Rows(0).Item(0)) Then
+            pro_id = dt.Rows(0).Item(0)
+            Me.pid.Text = pro_id + 1
+        Else
+            Me.pid.Text = 202400001
+        End If
+        conn.Close()
+    End Sub
+
+    Public Sub loadcat()
+        Call connect()
+        qry = "SELECT cat_name FROM category"
+        Dim da As New MySqlDataAdapter(qry, conn)
+        Dim dt As New DataTable()
+
+        da.Fill(dt)
+        category.Items.Clear()
+        For Each row As DataRow In dt.Rows
+            category.Items.Add(row("cat_name").ToString())
+
+        Next
+        conn.Close()
+    End Sub
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Call connect()
+        Dim cmd As New MySqlCommand
+        qry = "insert into products values('" & pid.Text & "','" & pname.Text & "','" & pbrand.Text &
+            "','" & category.Text & "','" & purprice.Text & "','" & mrp.Text & "','" & disc.Text & "','" & qty.Text & "',
+            '" & supplier.Text & "', curdate())"
+        cmd.CommandText = qry
+        'cmd.CommandType = CommandType.Text
+        cmd.Connection = conn
+        cmd.ExecuteNonQuery()
+        conn.Close()
+        Call showdata()
+        MsgBox("DATA SUCCESSFULLY INSERTED...")
+        pname.Clear()
+        pbrand.Clear()
+        mrp.Clear()
+        disc.Clear()
+        purprice.Clear()
+        qty.Clear()
+        supplier.Clear()
+        category.Text = ""
+        Call autoid()
+
+
+    End Sub
+
+
+
+    Private Sub DataGridView1_CellMouseClick(sender As Object, e As DataGridViewCellMouseEventArgs) Handles DataGridView1.CellMouseClick
+        If DataGridView1.SelectedCells.Count > 0 Then
+            Button2.Enabled = True
+            Button3.Enabled = True
+            MsgBox(DataGridView1.CurrentRow.Cells(0).Value.ToString)
+        Else
+            Button2.Enabled = False
+            Button3.Enabled = False
+        End If
+
+    End Sub
 End Class
