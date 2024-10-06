1a036b488ef14c1567534b0b3ee99780a69d45df
bagdi <111894690+Bagd21@users.noreply.github.com>
2024-10-06 16:07:40 +0530

    add form ,abhishek aap employee detail banao

---
+++ Employee.vb
@@ -0,0 +1,9 @@
+﻿Public Class Employee
+    Private Sub Employee_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim screenWidth As Integer = Screen.PrimaryScreen.Bounds.Width
+        Dim screenHeight As Integer = Screen.PrimaryScreen.Bounds.Height
+        Me.Size = New Size(screenWidth, screenHeight)
+        'Me.FormBorderStyle = FormBorderStyle.None
+        Me.WindowState = FormWindowState.Maximized
+    End Sub
+End Class
